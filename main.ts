9731f76cfd3553724ca9c134188855be5c099eca
Voidboi0163 <122555118+Voidboi0163@users.noreply.github.com>
2023-01-19 12:24:22 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,78 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+})
+input.onButtonPressed(Button.AB, function () {
+    while (true) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(_10)
+        basic.clearScreen()
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . # . # .
+            . . # . .
+            . . . . .
+            `)
+        basic.pause(_10)
+        basic.clearScreen()
+        basic.showLeds(`
+            . . # . .
+            . # . # .
+            # . . . #
+            . # . # .
+            . . # . .
+            `)
+        basic.pause(_10)
+        basic.clearScreen()
+        basic.showLeds(`
+            . # . # .
+            # . . . #
+            . . . . .
+            # . . . #
+            . # . # .
+            `)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . # . . .
+        # . # . .
+        . . # . .
+        . . # # .
+        . . # # .
+        `)
+    music.playMelody("C E D C G C E G ", 120)
+    music.playMelody("A C5 B A G C E G ", 120)
+    music.playMelody("F A G F E G F E ", 120)
+    music.playMelody("D G A B C5 B C5 C5 ", 120)
+})
+input.onGesture(Gesture.Shake, function () {
+    ball8 = randint(1, 3)
+    if (ball8 == 1) {
+        basic.showString("Yes")
+    } else if (ball8 == 2) {
+        basic.showString("No")
+    } else {
+        basic.showString("Maybe")
+    }
+})
+let ball8 = 0
+let _10 = 0
+_10 = 10
+basic.forever(function () {
+    if (input.rotation(Rotation.Pitch) >= 180) {
+        basic.clearScreen()
+    }
+})
