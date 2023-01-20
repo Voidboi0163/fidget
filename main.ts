60920af639168af9e657741ab3625020666dfda0
Voidboi0163 <122555118+Voidboi0163@users.noreply.github.com>
2023-01-20 12:06:30 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -24,6 +24,21 @@ input.onGesture(Gesture.ScreenDown, function () {
     basic.clearScreen()
     _true = 0
 })
+input.onSound(DetectedSound.Loud, function () {
+    for (let index = 0; index < 10; index++) {
+        basic.showString("Y", 5)
+basic.showString("N", 5)
+basic.showString("M", 5)
+    }
+    ball8 = randint(1, 3)
+    if (ball8 == 1) {
+        basic.showString("Y")
+    } else if (ball8 == 2) {
+        basic.showString("N")
+    } else {
+        basic.showString("M")
+    }
+})
 input.onButtonPressed(Button.AB, function () {
     _true = 1
     while (_true == 1) {
@@ -75,21 +90,6 @@ input.onButtonPressed(Button.B, function () {
     music.playMelody("A C5 B A G C E G ", 120)
     music.playMelody("F A G F E G F E ", 120)
     music.playMelody("D G A B C5 B C5 C5 ", 120)
-})
-input.onGesture(Gesture.Shake, function () {
-    for (let index = 0; index < 10; index++) {
-        basic.showString("Y", 5)
-basic.showString("N", 5)
-basic.showString("M", 5)
-    }
-    ball8 = randint(1, 3)
-    if (ball8 == 1) {
-        basic.showString("Y")
-    } else if (ball8 == 2) {
-        basic.showString("N")
-    } else {
-        basic.showString("M")
-    }
 })
 input.onGesture(Gesture.LogoDown, function () {
     basic.showLeds(`
